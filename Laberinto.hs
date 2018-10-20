9ad9cffcf2adb11d7cd95c9f55e816c2624944ff
sandra <sandrav0609@gmail.com>
2018-10-20 15:11:12 -0400

    Fix Laberinto Data type, add caminoSinSalida and crearTesoro constructors. Problem with replaicing Trifurcacion in agregarLaberinto.

--- Laberinto.hs
+++ Laberinto.hs
@@ -2,7 +2,26 @@ import Data.String
 
 {- Tipo de Datos: Laberinto 
 					Trifurcacion: derecha, izquierda, recto -}
-data Laberinto a = Trifurcacion (Maybe (Laberinto a)) (Maybe (Laberinto a)) (Maybe (Laberinto a))
+data Laberinto = Trifurcacion (Maybe (Laberinto)) (Maybe (Laberinto)) (Maybe (Laberinto))
+				| Tesoro String (Maybe (Laberinto))
+				deriving (Show)
 
-data Tesoro t = String t (Maybe (Laberinto t))
+{- Función que retorna un camino sin salida (Trifurcacion
+	donde todos los caminos conducen a Nothing)-}
+caminoSinSalida :: Maybe(Laberinto)
+caminoSinSalida = Just(Trifurcacion (Nothing) (Nothing) (Nothing))
 
+{- Función que recibe un String con la descripción de un tesoro y
+	un laberinto y retorna un Tesoro-}
+crearTesoro :: String -> Maybe(Laberinto) -> Maybe(Laberinto)
+crearTesoro s a = Just(Tesoro s a)
+
+{- Función que recibe una Trifurcacion, un laberinto y
+	un indicador de cual camino los relaciona y retorna 
+	una Trifurcacion en que se indique que dicho camino
+	conduce a dicho laberinto-}
+agregarLaberinto :: Maybe(Laberinto) -> Maybe(Laberinto) -> Char -> Maybe(Laberinto)
+agregarLaberinto trif lab _ = error "Se debe especificar dirección del camino ('d', 'i' o 'r')"
+agregarLaberinto trif lab 'd' = trif (lab) (_) (_)
+agregarLaberinto trif lab 'i' = trif (_) (lab) (_)
+agregarLaberinto trif lab 'r' = trif (_) (_) (lab)
