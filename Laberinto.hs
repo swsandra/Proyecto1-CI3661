884340b9da33b2f169dbbc22c4fba5defc7b87fb
sandra <sandrav0609@gmail.com>
2018-10-20 16:59:00 -0400

    Fix caminoSinSalida, agregarLaberinto and crearTesoro

--- Laberinto.hs
+++ Laberinto.hs
@@ -1,27 +1,43 @@
 import Data.String
 
 {- Tipo de Datos: Laberinto 
-					Trifurcacion: derecha, izquierda, recto -}
+					Trifurcacion: izquierda, recto, derecho -}
 data Laberinto = Trifurcacion (Maybe (Laberinto)) (Maybe (Laberinto)) (Maybe (Laberinto))
 				| Tesoro String (Maybe (Laberinto))
 				deriving (Show)
 
+{-Funciones de Construcción-}
+
 {- Función que retorna un camino sin salida (Trifurcacion
 	donde todos los caminos conducen a Nothing)-}
-caminoSinSalida :: Maybe(Laberinto)
-caminoSinSalida = Just(Trifurcacion (Nothing) (Nothing) (Nothing))
+caminoSinSalida :: Laberinto
+caminoSinSalida = Trifurcacion (Nothing) (Nothing) (Nothing)
 
 {- Función que recibe un String con la descripción de un tesoro y
 	un laberinto y retorna un Tesoro-}
-crearTesoro :: String -> Maybe(Laberinto) -> Maybe(Laberinto)
-crearTesoro s a = Just(Tesoro s a)
+crearTesoro :: String -> Laberinto -> Laberinto
+crearTesoro s a = Tesoro s (Just a)
 
 {- Función que recibe una Trifurcacion, un laberinto y
 	un indicador de cual camino los relaciona y retorna 
 	una Trifurcacion en que se indique que dicho camino
 	conduce a dicho laberinto-}
-agregarLaberinto :: Maybe(Laberinto) -> Maybe(Laberinto) -> Char -> Maybe(Laberinto)
-agregarLaberinto trif lab _ = error "Se debe especificar dirección del camino ('d', 'i' o 'r')"
-agregarLaberinto trif lab 'd' = trif (lab) (_) (_)
-agregarLaberinto trif lab 'i' = trif (_) (lab) (_)
-agregarLaberinto trif lab 'r' = trif (_) (_) (lab)
+agregarLaberinto :: Laberinto -> Laberinto -> Char -> Laberinto
+agregarLaberinto (Trifurcacion izq recto der) lab 'd' = Trifurcacion izq recto (Just lab)
+agregarLaberinto (Trifurcacion izq recto der) lab 'i' = Trifurcacion (Just lab) recto der
+agregarLaberinto (Trifurcacion izq recto der) lab 'r' = Trifurcacion izq (Just lab) der
+agregarLaberinto (Trifurcacion izq recto der) lab _ = error "Se debe especificar dirección del camino ('d', 'i' o 'r')"
+
+--Prueba caminoSinSalida
+--let lab1 = Trifurcacion (Just (Trifurcacion Nothing Nothing (Just (caminoSinSalida)))) (Just (caminoSinSalida)) (Just (Trifurcacion Nothing Nothing (Just (caminoSinSalida))))
+--Prueba crearTesoro
+--let lab2 = Trifurcacion (Just (Trifurcacion Nothing Nothing (Just (caminoSinSalida)))) (Just (crearTesoro "Tesoro1" caminoSinSalida)) (Just (crearTesoro "Tesoro2" caminoSinSalida))
+--Prueba agregarLaberinto
+--let lab3 = caminoSinSalida
+--Agregar los lab1 y lab2
+--let lab4 = agregarLaberinto lab3 lab1 'd' ESTO DA ALGO LARGUISIMO
+--Pruebas de agregarLaberinto con caminos sin salida
+--let lab4 = agregarLaberinto lab3 caminoSinSalida 'd'
+--Pruebalo con el primer parametro como un laberinto mas complejo (que si Trifurcacion Nothing Nothing (Just(caminoSinSalida)))
+
+{-Funciones de Acceso-}
