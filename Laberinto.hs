bc44b19c4447c1353aa246ae430e7ce92e097aa4
AuriCA32 <auri.git.17@gmail.com>
2018-10-29 05:48:37 -0400

    Added option to build from client program Modified exported functions in Laberinto.hs Add functions in Sabio.hs to give functionality "Buil maze from route given"

--- Laberinto.hs
+++ Laberinto.hs
@@ -1,8 +1,12 @@
+module Laberinto
+( Laberinto(..),
+  caminoSinSalida,
+  agregarLaberinto) where
 {- Tipo de Datos: Laberinto 
-					Trifurcacion: izquierda, recto, derecho -}
+                    Trifurcacion: izquierda, recto, derecho -}
 data Laberinto = Trifurcacion (Maybe (Laberinto)) (Maybe (Laberinto)) (Maybe (Laberinto))
-				| Tesoro String (Maybe (Laberinto))
-				deriving (Show)
+                | Tesoro String (Maybe (Laberinto))
+                deriving (Show)
 
 {-Instancias de Show y Read-}
 
@@ -10,19 +14,19 @@ data Laberinto = Trifurcacion (Maybe (Laberinto)) (Maybe (Laberinto)) (Maybe (La
 {-Funciones de Construcción-}
 
 {- Función que retorna un camino sin salida (Trifurcacion
-	donde todos los caminos conducen a Nothing)-}
+    donde todos los caminos conducen a Nothing)-}
 caminoSinSalida :: Laberinto
 caminoSinSalida = Trifurcacion (Nothing) (Nothing) (Nothing)
 
 {- Función que recibe un String con la descripción de un tesoro y
-	un laberinto y retorna un Tesoro-}
+    un laberinto y retorna un Tesoro-}
 crearTesoro :: [Char] -> Laberinto -> Laberinto
 crearTesoro s a = Tesoro s (Just a)
 
 {- Función que recibe una Trifurcacion, un laberinto y
-	un indicador de cual camino los relaciona y retorna 
-	una Trifurcacion en que se indique que dicho camino
-	conduce a dicho laberinto-}
+    un indicador de cual camino los relaciona y retorna 
+    una Trifurcacion en que se indique que dicho camino
+    conduce a dicho laberinto-}
 agregarLaberinto :: Laberinto -> Laberinto -> Char -> Laberinto
 agregarLaberinto (Trifurcacion izq recto der) lab 'd' = Trifurcacion izq recto (Just lab)
 agregarLaberinto (Trifurcacion izq recto der) lab 'i' = Trifurcacion (Just lab) recto der
@@ -45,33 +49,33 @@ agregarLaberinto (Trifurcacion izq recto der) lab _ = error "Se debe especificar
 {-Funciones de Acceso-}
 
 {- Función que recibe un laberinto y retorna el laberinto
-	que comienza al voltear a la izquierda-}
+    que comienza al voltear a la izquierda-}
 voltearIzquierda :: Laberinto -> Laberinto
 voltearIzquierda (Trifurcacion (Just(izq)) recto der) = izq
 voltearIzquierda (Trifurcacion _ recto der) = error "No hay camino"
 
 {- Función que recibe un laberinto y retorna el laberinto
-	que comienza al voltear a la derecha-}
+    que comienza al voltear a la derecha-}
 voltearDerecha :: Laberinto -> Laberinto
 voltearDerecha (Trifurcacion izq recto (Just(der))) = der
 voltearDerecha (Trifurcacion izq recto _) = error "No hay camino"
 
 {- Función que recibe un laberinto y retorna el laberinto
-	que comienza al seguir recto-}
+    que comienza al seguir recto-}
 irRecto :: Laberinto -> Laberinto
 irRecto (Trifurcacion izq (Just(recto)) der) = recto
 irRecto (Trifurcacion izq _ der) = error "No hay camino"
 
 {- Función que recibe un laberinto y una ruta y retorna el
-	laberinto que comienza en el punto al que conduce esa ruta
-	La ruta es un string con d,i o r si es derecha, izquierda
-	o recto respectivamente (e.g dirrdii)-}
+    laberinto que comienza en el punto al que conduce esa ruta
+    La ruta es un string con d,i o r si es derecha, izquierda
+    o recto respectivamente (e.g dirrdii)-}
 recorrerLaberinto :: Laberinto -> [Char] -> Laberinto
 recorrerLaberinto lab [] = lab
 recorrerLaberinto lab (x:xs) | x == 'i' = recorrerLaberinto (voltearIzquierda lab) xs
-							 | x == 'r' = recorrerLaberinto (irRecto lab) xs
-							 | x == 'd' = recorrerLaberinto (voltearDerecha lab) xs
-							 | otherwise = error "No se ha insertado la ruta correctamente"
+                             | x == 'r' = recorrerLaberinto (irRecto lab) xs
+                             | x == 'd' = recorrerLaberinto (voltearDerecha lab) xs
+                             | otherwise = error "No se ha insertado la ruta correctamente"
 
 --Pruebas con los laberintos de la prueba anterior
 --let lab4 = voltearIzquierda lab1
@@ -80,72 +84,72 @@ recorrerLaberinto lab (x:xs) | x == 'i' = recorrerLaberinto (voltearIzquierda la
 --let lab5 = agregarLaberinto lab2 lab1 'i'
 --Para este ejemplo puedes ver mas fácil cómo lo recorre aquí
 {- Trifurcacion 
-	(Just (Trifurcacion 
-			(Just (Trifurcacion 
-					Nothing 
-					Nothing 
-					(Just (Trifurcacion 
-							Nothing 
-							Nothing 
-							Nothing)))) 
-			(Just (Trifurcacion 
-					Nothing 
-					Nothing 
-					Nothing)) 
-			(Just (Trifurcacion 
-					Nothing 
-					Nothing 
-					(Just (Trifurcacion 
-							Nothing 
-							Nothing 
-							Nothing))))))
-	 (Just (Tesoro "Tesoro1" 
-	 		(Just (Trifurcacion 
-	 				Nothing 
-	 				Nothing 
-	 				Nothing)))) 
-	 (Just (Tesoro "Tesoro2" 
-	 		(Just (Trifurcacion 
-	 				Nothing 
-	 				Nothing 
-	 				Nothing))))
+    (Just (Trifurcacion 
+            (Just (Trifurcacion 
+                    Nothing 
+                    Nothing 
+                    (Just (Trifurcacion 
+                            Nothing 
+                            Nothing 
+                            Nothing)))) 
+            (Just (Trifurcacion 
+                    Nothing 
+                    Nothing 
+                    Nothing)) 
+            (Just (Trifurcacion 
+                    Nothing 
+                    Nothing 
+                    (Just (Trifurcacion 
+                            Nothing 
+                            Nothing 
+                            Nothing))))))
+     (Just (Tesoro "Tesoro1" 
+            (Just (Trifurcacion 
+                    Nothing 
+                    Nothing 
+                    Nothing)))) 
+     (Just (Tesoro "Tesoro2" 
+            (Just (Trifurcacion 
+                    Nothing 
+                    Nothing 
+                    Nothing))))
 
 Otro ejemplo con mas anidacion (Just(caminoSinSalida))
 let lab6 = Trifurcacion Nothing Nothing (Just(Trifurcacion (Just(Trifurcacion (Just(Trifurcacion (Just(caminoSinSalida)) Nothing (Just(Trifurcacion (Just(caminoSinSalida)) Nothing (Just(caminoSinSalida)))))) Nothing Nothing)) Nothing (Just(Trifurcacion (Just(caminoSinSalida)) Nothing (Just(caminoSinSalida))))))
  Trifurcacion 
- 	Nothing 
- 	Nothing 
- 	(Just (Trifurcacion 
- 		(Just (Trifurcacion 
- 			(Just (Trifurcacion 
- 				(Just (Trifurcacion 
- 						Nothing 
- 						Nothing 
- 						Nothing)) 
- 				Nothing 
- 				(Just (Trifurcacion 
- 					(Just (Trifurcacion 
- 							Nothing 
- 							Nothing 
- 							Nothing)) 
- 					Nothing
- 					(Just (Trifurcacion 
- 							Nothing 
- 							Nothing 
- 							Nothing)))))) 
- 			Nothing 
- 			Nothing)) 
- 		Nothing 
- 		(Just (Trifurcacion 
- 				(Just (Trifurcacion 
- 						Nothing 
- 						Nothing 
- 						Nothing)) 
- 				Nothing 
- 				(Just (Trifurcacion 
- 						Nothing 
- 						Nothing 
- 						Nothing))))))
+    Nothing 
+    Nothing 
+    (Just (Trifurcacion 
+        (Just (Trifurcacion 
+            (Just (Trifurcacion 
+                (Just (Trifurcacion 
+                        Nothing 
+                        Nothing 
+                        Nothing)) 
+                Nothing 
+                (Just (Trifurcacion 
+                    (Just (Trifurcacion 
+                            Nothing 
+                            Nothing 
+                            Nothing)) 
+                    Nothing
+                    (Just (Trifurcacion 
+                            Nothing 
+                            Nothing 
+                            Nothing)))))) 
+            Nothing 
+            Nothing)) 
+        Nothing 
+        (Just (Trifurcacion 
+                (Just (Trifurcacion 
+                        Nothing 
+                        Nothing 
+                        Nothing)) 
+                Nothing 
+                (Just (Trifurcacion 
+                        Nothing 
+                        Nothing 
+                        Nothing))))))
 -}
 
 
--- Sabio.hs
+++ Sabio.hs
@@ -1,7 +1,13 @@
-{-module Main (main) where
-import Laberinto-}
+module Main (main) where
+import Laberinto
 import Data.Char (digitToInt, isDigit)
+import Data.Typeable
 
+-- Crear laberinto vacio en memoria
+laberintoEnMem :: Laberinto
+laberintoEnMem = caminoSinSalida
+
+-- Definicion opciones de menu
 opcionesDisponibles :: [(String, String)]
 opcionesDisponibles = [
     ("1", "Comenzar a hablar de un laberinto nuevo"),
@@ -14,11 +20,34 @@ opcionesDisponibles = [
     ("8", "Hablar de un laberinto de nombre conocido")
     ]
 
-contertToString :: (String, String) -> String
-contertToString (numStr, str) = numStr ++ " .- " ++ str
-
+-- Funcion para imprimir menu
 printMenu :: IO ()
 printMenu = putStr $ foldl (\r (x,y) -> r ++ " " ++ x++ ".- " ++ y ++ "\n") "" opcionesDisponibles
+
+instruccionesRuta :: IO()
+instruccionesRuta = do
+    putStrLn "Para indicar una ruta, introduzca una cadena de caracteres."
+    putStrLn "Dichos caracteres sólo pueden ser:"    
+    putStrLn "  d : girar a la derecha"
+    putStrLn "  i : girar a la izquierda"
+    putStrLn "  r : seguir recto"
+    putStrLn "De introducir un caracter invalido se mostrará un mensaje de error"
+
+comenzarLaberintoNuevo :: IO()
+comenzarLaberintoNuevo = do
+    let laberintoEnMem = caminoSinSalida
+    putStrLn "Se ha borrado el laberinto en Memoria."
+    putStrLn "Indique una ruta para poblar el laberinto."
+    instruccionesRuta
+    i <- getLine
+    construirLaberinto i
+    putStrLn "Se ha poblado el laberinto"
+
+--construirLaberinto :: [Char] -> IO ()
+construirLaberinto (x:xs) = do
+    let laberintoEnMem = agregarLaberinto (caminoSinSalida) laberintoEnMem x
+    putStr ""
+    
 
 -- Funcion para mostrar las opciones y recibir del user
 mostrarRecibirOpciones :: IO ()
@@ -26,13 +55,15 @@ mostrarRecibirOpciones = do
     printMenu
     i:_ <- getLine
     if not ((isDigit i) && ((digitToInt i) `elem` [1..8])) then do
-        putStrLn "Operación incorrecta. Introduzca una opción válida."
+        putStrLn "La opción escogida incorrecta. Introduce una opción válida, viajero. \n"
     else
-        putStr "do something"
+        case (digitToInt i) of 
+            1 -> comenzarLaberintoNuevo
+            _ -> putStrLn "Do somth else"
     mostrarRecibirOpciones
-
 
 main :: IO ()
 main = do
-    putStrLn "¡Bienvenido! Escoge una opción"
+    putStrLn "¡Bienvenido, aventurero!"
+    putStrLn "Soy el sabio del laberinto. Adelante, escoge una opción"
     mostrarRecibirOpciones
